77712c3fe17ffd07e1528e76199e9b4e3ceace68
jadyms <33762825+jadyms@users.noreply.github.com>
2020-04-29 11:45:43 +0100

    Fetch multiple apis

--- scripts copy.js
+++ scripts copy.js
@@ -1,151 +1,111 @@
 //Initialize the object
 const httpRequest = new XMLHttpRequest(); 
-// httpRequest.responseType = "json";
-//URL parameters
+//URL parameters dictionary
 const API_URL= 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
 const API_KEY= '9e367c26-13d3-4ef2-9d5a-9ec3906204a4';
+// URL parameter thesaurus
+const API_URL_SYN= 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
+const API_KEY_SYN= '28bf78be-74f2-4f03-ad93-0ec25c9ac443';
+var word;
 
-//const word = 'apple';
-
-
-    
-
-// httpRequest.onreadystatechange = processRequest;
-
-// function processRequest(error){
-//     if (httpRequest.readyState == 4 && httpRequest.status == 200){
-
-//         console.log(httpRequest.response)
-//     }
-// }
-
+//Content of the page div
 const app = document.getElementById('box');
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
-// app.appendChild(container);
+
+//Get user input 
+function getWord(input) {
+     $("ul").empty();       //clean results from last search
+    word = $(input).val();  // grab text from input
+        $(input).val("");   //clear search bar
+    return word;
+}
+
+//Dictionary url
+function urlBuilder(word){
+    return url = `${API_URL}${word}&?key=${API_KEY}`;
+     
+}
+
+//THESAURUS url
+function urlBuilderThesaurus(word){
+    return urlThesaurus = `${API_URL_SYN}${word}&?key=${API_KEY_SYN}`;
+     
+}
+
+    //When user hit enter
+    $("input[type='text']").keypress(function(event){ //CHANGE IT TO BUTTON 
+           if(event.which===13){ 	//enter key = 13
+                word = getWord(this); //get user input
+                url = urlBuilder(word); //get the url
+                
+                //Request a GET method to DICTIONARY api asynchronously 
+                httpRequest.open('GET', url, true);
+                //Send the Request
+                httpRequest.send();
+            }
 
 
-
-$("input[type='text']").keypress(function(e){
-    	//enter key = 13
-    	if(e.which===13){
-            $("ul").empty();
-    		// grab text from input
-    	const word = $(this).val();
-            $(this).val("");
-           
-            const url = `${API_URL}${word}&?key=${API_KEY}`;
-
-            //Request a GET method to DICTIONARY api asynchronously 
-httpRequest.open('GET', url, true);
-
-//Send the Request
-httpRequest.send();
-
-    
-httpRequest.onload = function(){
-    var parsedData = JSON.parse(httpRequest.response);
-   console.log(httpRequest.status)
-   console.log("parsedData" + parsedData);
-    console.log("type of" + typeof parsedData);
-   console.log(Object.entries(parsedData).length > 0);
-    // console.log("MEta" + parsedData[0].meta);
-
-    if(Object.entries(parsedData).length < 0 ){
-        alert("We could not find this word");
-        return;
-    }else
-    if( parsedData[0].meta === undefined ){
-        alert("We could not find this word");
-        return;
-    } else{
-
-
-
-//     if(parsedData[0].meta === undefined || parsedData === undefined ){
-//         alert("We could not find this word");
-//         // $("ul").append("<li><strong>We could not find this word  </strong> </li>")
-// return;
-//     }
-
-   
-
-    // if(httpRequest.status >= 200 && httpRequest.status < 400){
-       
-        // console.log(typeof parsedData);
-        parsedData.forEach(data=>{
-           
-        // console.log(parsedData[0].meta);
-         
-           
-       
-        if (data === undefined || data.date === undefined) {
- data.date = "No date found";
- $("ul").append("<li><strong>"+ data.hwi.hw +"  </strong>"
-                                          + data.shortdef + 
-                                         "<br>"+ data.date + 
-                                       
-                                         "</li>")
-      
-                                        //  console.log(typeof data.date + data.date);
-       
-}else if    (Object.keys(parsedData).length == 0){
- console.log("Empty object");
-
-}else {
-  
-
-            // const card = document.createElement('div');
-            // card.setAttribute('  class', 'card');
-
-            // const h1 = document.createElement('h1');
-            //h1.textContent = data.date;
-           // container.appendChild(h1);
-            // $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+ data.hwi.hw +"<br>"+ data.date + "</li")
-        $("ul").append("<li><strong>"+ data.hwi.hw +"  </strong>"
-                                          + data.shortdef + 
-                                         "<br><strong>"+ data.date + 
-                                       
-                                         "</strong></li>")
-      
-                                         console.log(typeof data.date + data.date);
+        //In case the request could not be made
+        httpRequest.onerror = function() {
+            alert("The request could not be sent");
         }
-      
-    
-    })
-       
-    // }else{
-      
-        console.log('Error');
-    }
-   
 
-};
-    	}
     });
 
+    //When user hit SHIFT - for Thesaurus test only
+    $("input[type='text']").keypress(function(event){ //CHANGE IT TO BUTTON 
+        if(event.which===16){ 	//SHIFT key = 16
+             word = getWord(this); //get user input
+             url = urlBuilderThesaurus(word); //get the url
+             
+             //Request a GET method to DICTIONARY api asynchronously 
+             httpRequest.open('GET', url, true);
+             //Send the Request
+             httpRequest.send();
+         }
 
+
+     //In case the request could not be made
+     httpRequest.onerror = function() {
+         alert("The request could not be sent");
+     }
+
+ });
+
+    // When response is received
+    httpRequest.onload = function(){
+        //Wrap the response under JSON format
+        var parsedData = JSON.parse(httpRequest.response);
+
+        if(httpRequest.status === 0 || httpRequest.status >= 200 && httpRequest.status < 400){
+            
+            // Check if the reponse is an empty object
+            if(Object.entries(parsedData).length < 0 || Object.entries(parsedData).length == 0){
+                alert("ZERO MATCHES - We could not find this word");
+                return;
+            //Response is not empty, but instead, it is an array of
+            //suggested names 
+            }else if( parsedData[0].meta === undefined ){
+                alert("DID YOU MEAN" + parsedData[0] );
+                return;
+            }else{
+                printResults(parsedData);
+            }
+        
+        
+        }else{
+            alert("Oh no, something went wrong: " + httpRequest.status + " " + httpRequest.responseText);
+    }
+}
     
-    
 
-    
-
-
-
-
- 
-
-
-// $("input[type='text']").keypress(function(e){
-// 	//enter key = 13
-// 	if(e.which===13){
-// 		// grab text from input
-// 		var todoText = $(this).val();
-// 		$(this).val("");
-
-// 		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+ todoText + "</li")
-
-// 	}
-// });
-
-
+        function printResults(parsedData){
+            parsedData.forEach(data=>{
+                //We found the word but there is no first known date
+                 if (data === undefined || data.date === undefined) {
+                        data.date = "No date found";
+                        $("ul").append("<li><strong>"+ data.hwi.hw +"  </strong>"+ data.shortdef + "<br>"+ data.date + "</li>");     
+                }else{
+                    $("ul").append("<li><strong>"+ data.hwi.hw +"  </strong>"+ data.shortdef + "<br><strong>"+ data.date + "</strong></li>");
+                }
+        })
+    }
--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
+
 //Initialize the object
 const httpRequest = new XMLHttpRequest(); 
-//URL parameters
+//URL parameters dictionary
 const API_URL= 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
 const API_KEY= '9e367c26-13d3-4ef2-9d5a-9ec3906204a4';
+// URL parameter thesaurus
+const API_URL_SYN= 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/'
+const API_KEY_SYN= '28bf78be-74f2-4f03-ad93-0ec25c9ac443';
 var word;
+
+var urls;
 
 //Content of the page div
 const app = document.getElementById('box');
@@ -16,56 +22,140 @@ function getWord(input) {
     return word;
 }
 
+//Dictionary url
 function urlBuilder(word){
     return url = `${API_URL}${word}&?key=${API_KEY}`;
      
 }
 
+//THESAURUS url
+function urlBuilderThesaurus(word){
+    return urlThesaurus = `${API_URL_SYN}${word}&?key=${API_KEY_SYN}`;
+     
+}
+
+   
+
     //When user hit enter
     $("input[type='text']").keypress(function(event){ //CHANGE IT TO BUTTON 
-           if(event.which===13){ 	//enter key = 13
+        var keypress =   event.which;
+        if(keypress===13){ 	//enter key = 13
                 word = getWord(this); //get user input
                 url = urlBuilder(word); //get the url
-                
-                //Request a GET method to DICTIONARY api asynchronously 
-                httpRequest.open('GET', url, true);
-                //Send the Request
-                httpRequest.send();
+                urlThesaurus = urlBuilderThesaurus(word);
+
+                urls = [url, urlThesaurus];
+                getPromisses(urls, keypress);
+
+           }
+
+           function getPromisses(urls, keypress){
+
+
+                // use map() to perform a fetch and handle the response for each url
+Promise.all(urls.map(url =>
+    fetch(url)
+      .then(checkStatus)                 
+      .then(parseJSON)
+      .catch(error => console.log('There was a problem!', error))
+  ))
+  .then(data => {
+      if(keypress===13){
+        const parsedData = data[0];
+        dictionary(parsedData);
+
+      }
+   
+    const thesaurusData = data[1];
+    // do something with the data
+    //Wrap the response under JSON format
+    // var parsedData = JSON.parse(httpRequest.response);
+
+   
+  })
+                // //Request a GET method to DICTIONARY api asynchronously 
+                // httpRequest.open('GET', url, true);
+                // //Send the Request
+                // httpRequest.send();
             }
 
 
-        //In case the request could not be made
-        httpRequest.onerror = function() {
-            alert("The request could not be sent");
-        }
+        // //In case the request could not be made
+        // httpRequest.onerror = function() {
+        //     alert("The request could not be sent");
+        // }
 
     });
 
-    // When response is received
-    httpRequest.onload = function(){
-        //Wrap the response under JSON format
-        var parsedData = JSON.parse(httpRequest.response);
 
-        if(httpRequest.status === 0 || httpRequest.status >= 200 && httpRequest.status < 400){
-            
-            // Check if the reponse is an empty object
-            if(Object.entries(parsedData).length < 0 || Object.entries(parsedData).length == 0){
-                alert("ZERO MATCHES - We could not find this word");
-                return;
-            //Response is not empty, but instead, it is an array of
-            //suggested names 
-            }else if( parsedData[0].meta === undefined ){
-                alert("DID YOU MEAN" + parsedData[0] );
-                return;
-            }else{
-                printResults(parsedData);
-            }
+// ------------------------------------------
+//  HELPER FUNCTIONS
+// ------------------------------------------
+
+
+function dictionary(parsedData){
+    if(httpRequest.status === 0 || httpRequest.status >= 200 && httpRequest.status < 400){
         
-        
+        // Check if the reponse is an empty object
+        if(Object.entries(parsedData).length < 0 || Object.entries(parsedData).length == 0){
+            alert("ZERO MATCHES - We could not find this word");
+            return;
+        //Response is not empty, but instead, it is an array of
+        //suggested names 
+        }else if( parsedData[0].meta === undefined ){
+            alert("DID YOU MEAN" + parsedData[0] );
+            return;
         }else{
-            alert("Oh no, something went wrong: " + httpRequest.status + " " + httpRequest.responseText);
-    }
+            printResults(parsedData);
+            // printResults(thesaurusData);
+      
+        }
+    
+    
+    }else{
+        alert("Oh no, something went wrong: " + httpRequest.status + " " + httpRequest.responseText);
 }
+    
+}
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function parseJSON(response) {
+    return response.json();
+  }
+
+
+    
+    // When response is received
+//     httpRequest.onload = function(){
+//         //Wrap the response under JSON format
+//         var parsedData = JSON.parse(httpRequest.response);
+
+//         if(httpRequest.status === 0 || httpRequest.status >= 200 && httpRequest.status < 400){
+            
+//             // Check if the reponse is an empty object
+//             if(Object.entries(parsedData).length < 0 || Object.entries(parsedData).length == 0){
+//                 alert("ZERO MATCHES - We could not find this word");
+//                 return;
+//             //Response is not empty, but instead, it is an array of
+//             //suggested names 
+//             }else if( parsedData[0].meta === undefined ){
+//                 alert("DID YOU MEAN" + parsedData[0] );
+//                 return;
+//             }else{
+//                 printResults(parsedData);
+//             }
+        
+        
+//         }else{
+//             alert("Oh no, something went wrong: " + httpRequest.status + " " + httpRequest.responseText);
+//     }
+// }
     
 
         function printResults(parsedData){
